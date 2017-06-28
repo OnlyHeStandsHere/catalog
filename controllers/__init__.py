f3bf61420266a37f8ae5d5b090e37be273981953
Jesse Maitland <jesse@confirmedautomation.com>
2017-06-27 22:19:33 -0700

    finished simple styling

--- controllers/__init__.py
+++ controllers/__init__.py
@@ -1,1 +1,17 @@
+# controllers __init__ file
+from flask_httpauth import HTTPBasicAuth
+
+# location of the google log in secrets file for OAuth2
 CLIENT_SECRET_FILE = "./google/google_login.json"
+
+# this allows the auth object to be imported to
+# controller files as it is needed
+auth = HTTPBasicAuth()
+
+
+# create the verify_password callback in the controller __init__ so it
+# will be available across all controller files that import auth
+@auth.verify_password
+def verify_pw(username, password):
+    print('verifying password')
+    return True
--- controllers/api_cont.py
+++ controllers/api_cont.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, jsonify
 from models.restaurants import Restaurant
 from models.menu_items import MenuItem
+from controllers import auth
 
 api = Blueprint("api", __name__)
 
 
 @api.route("/restaurants/json")
+@auth.login_required
 def restaurants_json():
     restaurants = Restaurant.query.all()
     return jsonify(restaurants=[r.serialize_menu_items() for r in restaurants])
+
